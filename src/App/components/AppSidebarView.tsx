df99776d919740b748729ddb0e40f003a4010157
caines1989 <6423872+caines1989@users.noreply.github.com>
2022-07-25 19:12:30 +0100

    adding sidebarview

--- src/App/components/AppSidebarView.tsx
+++ src/App/components/AppSidebarView.tsx
@@ -4,19 +4,15 @@ import { APP_HEADER_HEIGHT } from "../constants/APP_HEADER_HEIGHT";
 import { COLOR } from "../constants/COLOR";
 import { SIDEBAR_WIDTH } from "../constants/SIDEBAR_WIDTH";
 
-
 export const AppSidebarView: FC = () => {
-  
-    return (
-        <Box
-        width={SIDEBAR_WIDTH}
-        overflow="auto"
-        height={`calc(100vh - ${APP_HEADER_HEIGHT}px)`}
-        sx={{ boxShadow: 3 }}
-        bgcolor={COLOR.gray[100]}
-        borderColor={COLOR.gray[200]}
-        >
-            
-        </Box>
-    );
+  return (
+    <Box
+      width={SIDEBAR_WIDTH}
+      overflow="auto"
+      height={`calc(100vh - ${APP_HEADER_HEIGHT}px)`}
+      sx={{ boxShadow: 3 }}
+      bgcolor={COLOR.gray[100]}
+      borderColor={COLOR.gray[200]}
+    ></Box>
+  );
 };
